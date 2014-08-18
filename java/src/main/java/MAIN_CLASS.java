4f45dc4958bf45ac79e02642b38fa94a1b19c53b
Tim Mattison <tim@mattison.org>
2014-08-18 10:08:43 -0400

    Added missing ParseException

--- java/src/main/java/MAIN_CLASS.java
+++ java/src/main/java/MAIN_CLASS.java
@@ -11,7 +11,7 @@ public class MAIN_CLASS {
     /* COMMONS_CLI_START */
     /* Example command line processing with commons CLI */
     /*
-    private static void processCommandLineOptions(String[] args) {
+    private static void processCommandLineOptions(String[] args) throws ParseException {
     	final String INPUT = "input";
     	final String OUTPUT = "output";
     	final String INPUT_FILE_NAME = "input file name";
